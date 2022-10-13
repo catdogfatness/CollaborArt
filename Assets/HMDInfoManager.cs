7687bb629437ae7b89e554bf26d6cf868fa118e0
rou <rouliao0258@gmail.com>
2022-10-13 10:23:43 -0400

    update HMD infoManager

--- Assets/HMDInfoManager.cs
+++ Assets/HMDInfoManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class HMDInfoManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Is Device Active " + XRSettings.isDeviceActive);
+        Debug.Log("Device Name " + XRSettings.loadedDeviceName);
     }
 
     // Update is called once per frame
