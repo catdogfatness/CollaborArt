be02a8371c5bf2ae8e9e31cb37520697f3b936ac
SitongLiu-May <102379002+SitongLiu-May@users.noreply.github.com>
2022-11-07 13:15:42 -0500

    initialize

---
+++ Assets/Erase/Script/whiteboard.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whiteboard1 : MonoBehaviour
+{
+    public Texture2D texture;
+    public Vector2 textureSize = new Vector2(2048, 2048);
+
+    void Starts()
+    {
+        var r = GetComponent<Renderer>();
+        texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        r.material.mainTexture = texture;
+    }
+}
